b91cadc7027c902bc34ff2e7a5ace46a7334c9e5
Akhandalmanimalik <akhandalmanimalik@gmail.com>
2016-11-09 00:00:37 +0530

    added model,services and dao package with farmer model object class.

---
+++ krushikalyan/src/main/java/com/kk/model/Farmer.java
@@ -0,0 +1,40 @@
+package com.kk.model;
+
+public class Farmer {
+private String farmerName;
+private Double mobile;
+private String address;
+private String city;
+private Double pin;
+public String getFarmerName() {
+	return farmerName;
+}
+public void setFarmerName(String farmerName) {
+	this.farmerName = farmerName;
+}
+public Double getMobile() {
+	return mobile;
+}
+public void setMobile(Double mobile) {
+	this.mobile = mobile;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public Double getPin() {
+	return pin;
+}
+public void setPin(Double pin) {
+	this.pin = pin;
+}
+
+}
